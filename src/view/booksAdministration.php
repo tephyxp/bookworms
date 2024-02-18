606f927872669dc4a613ae24915e29383cd3bba3
vicki-robertson <135024584+vicki-robertson@users.noreply.github.com>
2024-02-18 14:13:54 +0100

    header added to book admin page

--- src/view/booksAdministration.php
+++ src/view/booksAdministration.php
@@ -49,19 +49,22 @@ if (isset($_GET['id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BookWorms Admin</title>
+
+    <link rel="stylesheet" href="../../resources/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;700&display=swap">
 
 </head>
 
-<body>
-    <header>
-        <img src="" alt="">
+<body class="custom-bd">
+    <header class="my-3 mx-5 d-flex justify-content-between align-items-center">
+        <img src="../../resources/img/bookworms.png" alt="Logo" class="logo">
         <form action="" method="post">
-            <input type="submit" name="logout" value="Cerrar Sesion">
+            <input type="submit" name="logout" value="Cerrar Sesion" class="btn btn-primary mb-3 secondary-button">
         </form>
     </header>
     <main>
-        <h1>Bookworms</h1>
+        <h1 class="m-3 text-center my-5 first-title">BOOKWORMS</h1>
         <div class="row">
             <div class="col-sm-12">
                 <?php if (isset($error)) : ?>
@@ -73,23 +76,36 @@ if (isset($_GET['id'])) {
             </div>
         </div>
         <form action="" method="post" enctype="multipart/form-data" class="form-control">
-            <div>
-                <label for="title" class="form-control">Titulo:
-                    <input class="form-control" type="text" name="title">
-                </label>
-                <label for="author">Autor:
-                    <input type="text" name="author">
-                </label>
-                <label for="isbn">ISBN:
-                    <input type="text" name="isbn"> </input>
-                </label>
-                <label for="image">Imagen:
-                    <input type="file" name="image"> </input>
-                </label>
-                <label for="description">Descripción:
-                    <textarea name="description"> </textarea>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="mb-3">
+                            <label for="title" class="custom-input mb-2">Titulo:
+                                <input class="form-control custom-input mb-2" type="text" name="title">
+                            </label>
+                        </div>
+                        <div class="mb-3">
+                            <label for="author">Autor:
+                                <input type="text" name="author" class="custom-input form-control mb-2">
+                            </label>
+                        </div>
+                        <label for="isbn">ISBN:
+                            <input type="text" name="isbn" class="custom-input form-control mb-2"> </input>
+                        </label>
+                    </div>
+                    <div class="col-md-6">
+                        <label for="image">Imagen:
+                            <input type="file" name="image" class="mb-2"> </input>
+                        </label>
+                        <label for="description">Descripción:
+                            <textarea name="description" class="custom-input mb-2"> </textarea>
+                        </label>
+                    </div>
+                    <div class="col-md-12 text-center mt-3">    
+                        <button type="submit" name="addBook" class="btn btn-primary mb-3 custom-button primary-button ">Añadir</button>
+                    </div>
+                </div>
             </div>
-            <button type="submit" name="addBook">Añadir</button>
         </form>
 
         <input type="search" id="search" placeholder="Buscar..." />
@@ -122,6 +138,7 @@ if (isset($_GET['id'])) {
             </div>
         </section>
     </main>
+
 
     <?php
     require_once __DIR__ . '/head/footer.php';
