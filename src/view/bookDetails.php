8947dcb6f7b76cfb0d767f0c94bf688553c44d75
ImLauraGS <lgilsolano@gmail.com>
2024-02-19 10:01:42 +0100

    styles book details done!

--- src/view/bookDetails.php
+++ src/view/bookDetails.php
@@ -17,39 +17,38 @@ $book = $bookController->getBookDetails($bookId);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bookworms Biblioteca</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../resources/styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;700&display=swap">
+    <link rel="stylesheet" href= "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <style>
-    .custom-blue-text {
-      color: #073060;
-      /* font-family:'Roboto-Slab, serif'; */
-    }
-    .paragraph-text {
-      color: #073060;
-      text-align: justify;
-      /* font-family:'Roboto-Slab, serif'; */
-    }
     body {
       background-color: #F6EEE0;
     }
     </style>
   
 </head>
-  <body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <article class="text-center w-50 m-auto">
-    <h2 class="custom-blue-text">BOOKWORMS</h2>
-
-    <img src="data:image/jpeg;base64,<?=base64_encode($book['image']) ?>" alt="Book Image" class="img-fluid w-20 h-40">
-
-    <h3 class="custom-blue-text mt-4"><?= $book["title"];?></h3>
-    <h4 class="custom-blue-text mt-3"><?= $book["author"];?></h4>
-    <p class="paragraph-text mt-3"><?= $book["description"];?></p>
-    </article>
+  <body class="custom-bd">
+  <header class="my-3 mx-5 d-flex justify-content-between align-items-center">
+        <a href="../../index.php">
+            <img src="../../resources/img/bookworms.png" alt="Logo" class="logo">
+        </a>
+        <div class="form-inline mb-3 d-flex justify-content-around align-items-center">
+            <a href="../../index.php" class="btn btn-primary ml-2 secondary-button me-2">Home</a>
+        </div>
+    </header>
+    <main class="container">
+      <article class="d-flex justify-content-center align-items-center flex-column">
+        <h1 class="m-3 text-center my-5 first-title">BOOKWORMS</h1>
+        <img src="data:image/jpeg;base64,<?=base64_encode($book['image']) ?>" alt="Book Image" class="img-fluid w-20 h-40 m-4">
+        <h3 class="custom-blue-text mt-4"><?= $book["title"];?></h3>
+        <h4 class="custom-blue-text mt-3"><?= $book["author"];?></h4>
+        <p class="paragraph-text mt-3 text-center" style="width: 50%;"><?= $book["description"];?></p>
+      </article>
+    </main>
   
     <?php
-        require_once __DIR__ . '/head/footer.php';
+    require_once __DIR__ . '/head/footer.php';
     ?>
 
 
