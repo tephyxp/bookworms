952ca6214a8d0c6cf13f8036c1cd4e1e48339a3b
melitacasola <melissacasola@gmail.com>
2024-02-19 22:57:03 +0100

    title page administration in head

--- src/view/booksAdministration.php
+++ src/view/booksAdministration.php
@@ -83,7 +83,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BookWorms Admin</title>
+    <title>Bookworms Biblioteca Admin</title>
 
     <link rel="stylesheet" href="../../resources/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
