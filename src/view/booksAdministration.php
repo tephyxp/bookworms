68f5aac4b5b4dbb1b6501c1867986e1093bbcfbd
vicki-robertson <135024584+vicki-robertson@users.noreply.github.com>
2024-02-19 10:07:53 +0100

    initial commit

--- src/view/booksAdministration.php
+++ src/view/booksAdministration.php
@@ -54,16 +54,14 @@ if (isset($_GET['id'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;700&display=swap">
     <link rel="stylesheet" href= "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <style>
-        .form-control {
-            display: grid !important;
-        }
-    </style>
+    
 </head>
 
 <body class="custom-bd">
     <header class="my-3 mx-5 d-flex justify-content-between align-items-center">
-        <img src="../../resources/img/bookworms.png" alt="Logo" class="logo">
+        <a href="../../index.php">
+            <img src="../../resources/img/bookworms.png" alt="Logo" class="logo" href="./index.php">
+        </a>
         <form action="" method="post">
             <input type="submit" name="logout" value="Cerrar Sesion" class="btn btn-primary mb-3 secondary-button">
         </form>
