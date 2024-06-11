defed9ad0c971afcb679a89b7e4b0ccae159479b
lolamindi <laura_mday@yahoo.co.uk>
2024-06-12 00:48:06 +0200

    change font

--- index.php
+++ index.php
@@ -40,7 +40,9 @@ if (!empty($searchKeyword)) {
     <link rel="stylesheet" href="./resources/styles.css">
     <link rel="icon" type="image/png" href="./resources/img/favicon.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;700&display=swap">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100..900&display=swap" rel="stylesheet">
 
 </head>
 
