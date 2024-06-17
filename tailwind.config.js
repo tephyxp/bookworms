5a6e6db0ab1b9028f3196cf141cca205c9ba63a1
lolamindi <laura_mday@yahoo.co.uk>
2024-06-17 21:46:35 +0200

    start adding tailwind styles

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.php', // This includes all PHP files
+    './**/*.html', // Include HTML files if you have any
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
