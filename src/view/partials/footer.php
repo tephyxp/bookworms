f86cfaba5994de3fb13e7d9470c5d6d1404c5d18
lolamindi <laura_mday@yahoo.co.uk>
2024-07-28 20:46:26 +0200

    change year on footer to dynamic content

--- src/view/partials/footer.php
+++ src/view/partials/footer.php
@@ -1,6 +1,4 @@
-
-
-<footer class="bg-lilac h-12 p-2">© 2024, Recs. book recommendations</footer>
+<footer class="bg-lilac h-12 p-2">© <?php echo date("Y"); ?>, Recs. book recommendations</footer>
 
 </body>
 </html>
